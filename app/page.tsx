316adf0ee72490d59a3db0268d5b9050b43f63c0
Tiago Diniz <tiagodnzz@gmail.com>
2026-01-21 13:36:29 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from './_components/header';
 import { Button } from './_components/ui/button';
 import { Input } from './_components/ui/input';
 import Image from 'next/image';
+import { Card, CardContent } from './_components/ui/card';
+import { Badge } from './_components/ui/badge';
+import { Avatar, AvatarImage } from './_components/ui/avatar';
 
 const Home = () => {
   return <div>
@@ -31,6 +34,31 @@ const Home = () => {
           alt="Banner"
         />
       </div>
+
+      {/* Agendamento */}
+      <Card className="mt-6">
+        <CardContent className="flex justify-between">
+          {/* esquerda */}
+          <div className="flex flex-col gap-2">
+            <Badge className="w-fit">Agendamento</Badge>
+            <h3 className="font-semibold">Corte de cabelo</h3>
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+              </Avatar>
+              <p>Barber Shop</p>
+            </div>
+          </div>
+
+          {/* direita */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Janeiro</p>
+            <p className="text-2xl font-bold">31</p>
+            <p className="text-sm">17:00</p>
+          </div>
+
+        </CardContent>
+      </Card>
     </div>
   </div>
 };
